23b3555c3f4c1e241f1596899315b6aacd6b9685
mahadglitch <mahadbinkyser@gmail.com>
2026-01-12 15:08:15 +0600

    lesson15

--- lesson15/activity1.py
+++ lesson15/activity1.py
@@ -1,18 +1,18 @@
-#PIL (Python Imaging Library) provides image editing capability to the python interpreter
+
 from tkinter import *
 from PIL import Image, ImageTk
 
-#PIL - pillow library
+# PIL - pillow library
 
-#Create a window with a title bar and set its geametry as well
+# Create a window with a title bar and set its geametry as well
 root = Tk()
 root.title("image")
 root.geometry("800x700")
-#Now use Image.open to open and identify the given image file.
+# Now use Image.open to open and identify the given image file.
 upload = Image.open("image.jpg")
-#Convert this Image to Tkinter compatible image
+# Convert this Image to Tkinter compatible image
 image = ImageTk.PhotoImage(upload)
-#Add image to Tkinter label
+# Add image to Tkinter label
 label = Label(root, image=image, height=700, width=800)
 label.place(x=50, y=0)
 label2 = Label(root, text="This is how you add image in Tkinter window")
