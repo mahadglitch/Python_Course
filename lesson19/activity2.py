08fa63ed6de5f3f3d08b616c361f84a44198e983
mahadglitch <mahadbinkyser@gmail.com>
2026-01-12 13:48:55 +0600

    HCF calculation

--- lesson19/activity2.py
+++ lesson19/activity2.py
@@ -1,8 +1,8 @@
 small = int(input("Enter your smallest number: "))
 high = int(input("Enter your largest number:"))
 
-while(small):
+while (small):
     small_new = small
-    small = high%small
+    small = high % small
     high = small_new
-print("The HCF is : ",high)    
+print("The HCF is : ", high)
