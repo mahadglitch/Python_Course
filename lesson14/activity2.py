e9113eb13fe7e670dbca8811fc5213be6d015563
mahadglitch <mahadbinkyser@gmail.com>
2026-01-12 15:08:05 +0600

    lesson14

--- lesson14/activity2.py
+++ lesson14/activity2.py
@@ -2,15 +2,16 @@ import tkinter as tik
 
 window = tik.Tk()
 
-for i in range(4): # rows
-    for j in range(4): # columns
+for i in range(4):  # rows
+    for j in range(4):  # columns
         frame = tik.Frame(
             master=window,
-            relief=tik.RAISED,
+            relief=tik.RAIactivity3SED,
             borderwidth=8
         )
-        frame.grid(row=i,column=j,padx=10,pady=10) # padding in x-axis and padding in y-axis
-        label = tik.Label(master=frame,text=f"Row {i}\nColumn {j}")
+        # padding in x-axis and padding in y-axis
+        frame.grid(row=i, column=j, padx=10, pady=10)
+        label = tik.Label(master=frame, text=f"Row {i}\nColumn {j}")
         label.pack()
 
-window.mainloop()        
+window.mainloop()
